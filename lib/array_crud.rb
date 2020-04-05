03f8f7c49411b71c230adda685e7241453efbc60
Grace Berman <grace.g.berman@gmail.com>
2020-04-05 15:53:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  names = ["Grace", "Avi", "Abby", "Barry"]
+  friends = ["Grace", "Avi", "Abby", "Barry"]
 end
 
 def add_element_to_end_of_array(array, element)
